1eaedd2c4cea1a9b253c643e44c1796d4255690c
João Inácio <egnawake@gmail.com>
2022-03-24 12:26:19 +0000

    Complete ex. 4.3

--- Chronos/Program.cs
+++ Chronos/Program.cs
@@ -1,4 +1,12 @@
-﻿using System;
+using System;
+
+// ex 4.3. Stopwatch class
+// ---------------
+// namespace: System.Diagnostics
+// property for elapsed time in milliseconds: ElapsedMilliseconds
+// property for checking if timer is running: IsRunning
+// method for starting timer: Start
+// method for stopping timer: Stop
 
 namespace Chronos
 {
