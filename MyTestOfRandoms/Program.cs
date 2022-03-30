abee97dca13ec0326a7bbe900c59341d4cd33eac
João Inácio <egnawake@gmail.com>
2022-03-30 21:49:45 +0100

    MyTestOfRandoms: roll dice and get sum of results

--- MyTestOfRandoms/Program.cs
+++ MyTestOfRandoms/Program.cs
@@ -9,6 +9,20 @@ namespace MyTestOfRandoms
             // ask user for number of dice to roll
             Console.Write("Enter number of dice to roll: ");
             int diceToRoll = int.Parse(Console.ReadLine());
+
+            // init RNG
+            Random rnd = new Random();
+
+            // roll dice and find the sum of all the values
+            int sumOfDiceValues = 0;
+            for (int i = 0; i < diceToRoll; i++)
+            {
+                int value = rnd.Next(1, 7);
+                sumOfDiceValues += value;
+            }
+
+            // print sum
+            Console.WriteLine($"The sum of all dice values is {sumOfDiceValues}");
         }
     }
 }
